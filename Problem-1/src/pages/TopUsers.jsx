7789e713c82fa44698cfab9a4d324f54ab7fc477
Rehan1818 <128110806+Tushar7436@users.noreply.github.com>
2025-03-28 11:39:39 +0530

    Endpoints testing
    tested all the endpoints

--- Problem-1/src/pages/TopUsers.jsx
+++ Problem-1/src/pages/TopUsers.jsx
@@ -12,7 +12,7 @@ function TopUsers() {
       try {
         setErrors(false);
         setLoading(true);
-        const Access = { access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzMTM2ODY1LCJpYXQiOjE3NDMxMzY1NjUsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjM3ZjJlNzBlLTRkN2EtNDY4Zi1iZDVmLTFmYjcyNTFkMmI2MSIsInN1YiI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiJ9LCJjb21wYW55TmFtZSI6IkFGRk9SRE1FRCIsImNsaWVudElEIjoiMzdmMmU3MGUtNGQ3YS00NjhmLWJkNWYtMWZiNzI1MWQyYjYxIiwiY2xpZW50U2VjcmV0IjoiZ0hmRGx4dkJHZE5IZGN0biIsIm93bmVyTmFtZSI6IlR1c2hhciBBZ2Fyd2FsIiwib3duZXJFbWFpbCI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiIsInJvbGxObyI6IjIyQkNFMTAyNDIifQ.4Wk4yvPRqww6KO9EmwaM1YAYdGxMDTFbkg_h1pHVJY4"};
+        const Access = {access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzMTQyMTYzLCJpYXQiOjE3NDMxNDE4NjMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjM3ZjJlNzBlLTRkN2EtNDY4Zi1iZDVmLTFmYjcyNTFkMmI2MSIsInN1YiI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiJ9LCJjb21wYW55TmFtZSI6IkFGRk9SRE1FRCIsImNsaWVudElEIjoiMzdmMmU3MGUtNGQ3YS00NjhmLWJkNWYtMWZiNzI1MWQyYjYxIiwiY2xpZW50U2VjcmV0IjoiZ0hmRGx4dkJHZE5IZGN0biIsIm93bmVyTmFtZSI6IlR1c2hhciBBZ2Fyd2FsIiwib3duZXJFbWFpbCI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiIsInJvbGxObyI6IjIyQkNFMTAyNDIifQ.5noAC46kuqppKHP6xp_MGbjIIzwFDp4iS_V1rsw_bFM.4Wk4yvPRqww6KO9EmwaM1YAYdGxMDTFbkg_h1pHVJY4"};
         const response = await axios.get("http://20.244.56.144/test/users",Access);
         console.log(response.data); 
         setUser(response.data);
