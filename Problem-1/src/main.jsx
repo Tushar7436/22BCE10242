1621dd8895c75889609bd6e5eed4b79cbde4859a
Rehan1818 <128110806+Tushar7436@users.noreply.github.com>
2025-03-28 11:32:38 +0530

    v1_updates

--- Problem-1/src/main.jsx
+++ Problem-1/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import TopUsers from "./pages/TopUsers.jsx";
+import Feed from "./pages/Feed.jsx";
+import TrendingPosts from "./pages/TrendingPosts.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/TopUsers",
+    element: <TopUsers />,
+  },
+  {
+    path: "/Feed",
+    element: <Feed />,
+  },
+  {
+    path: "/TrendingPosts",
+    element: <TrendingPosts />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
