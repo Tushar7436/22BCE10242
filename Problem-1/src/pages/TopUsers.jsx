9bfa880d9836df00beb652ca38c675d1db11465e
Rehan1818 <128110806+Tushar7436@users.noreply.github.com>
2025-03-28 11:47:21 +0530

    Update TopUsers.jsx
    Sorted the TopUsers by posts count

--- Problem-1/src/pages/TopUsers.jsx
+++ Problem-1/src/pages/TopUsers.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../App.css"
+import "../App.css";
 
 function TopUsers() {
-  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
   const [errors, setErrors] = useState(false);
   const [loading, setLoading] = useState(true);
 
@@ -12,11 +12,17 @@ function TopUsers() {
       try {
         setErrors(false);
         setLoading(true);
-        const Access = {access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzMTQyMTYzLCJpYXQiOjE3NDMxNDE4NjMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjM3ZjJlNzBlLTRkN2EtNDY4Zi1iZDVmLTFmYjcyNTFkMmI2MSIsInN1YiI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiJ9LCJjb21wYW55TmFtZSI6IkFGRk9SRE1FRCIsImNsaWVudElEIjoiMzdmMmU3MGUtNGQ3YS00NjhmLWJkNWYtMWZiNzI1MWQyYjYxIiwiY2xpZW50U2VjcmV0IjoiZ0hmRGx4dkJHZE5IZGN0biIsIm93bmVyTmFtZSI6IlR1c2hhciBBZ2Fyd2FsIiwib3duZXJFbWFpbCI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiIsInJvbGxObyI6IjIyQkNFMTAyNDIifQ.5noAC46kuqppKHP6xp_MGbjIIzwFDp4iS_V1rsw_bFM.4Wk4yvPRqww6KO9EmwaM1YAYdGxMDTFbkg_h1pHVJY4"};
-        const response = await axios.get("http://20.244.56.144/test/users",Access);
-        console.log(response.data); 
-        setUser(response.data);
+
+        const access_token =
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzMTM2ODY1LCJpYXQiOjE3NDMxMzY1NjUsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjM3ZjJlNzBlLTRkN2EtNDY4Zi1iZDVmLTFmYjcyNTFkMmI2MSIsInN1YiI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiJ9LCJjb21wYW55TmFtZSI6IkFGRk9SRE1FRCIsImNsaWVudElEIjoiMzdmMmU3MGUtNGQ3YS00NjhmLWJkNWYtMWZiNzI1MWQyYjYxIiwiY2xpZW50U2VjcmV0IjoiZ0hmRGx4dkJHZE5IZGN0biIsIm93bmVyTmFtZSI6IlR1c2hhciBBZ2Fyd2FsIiwib3duZXJFbWFpbCI6InR1c2hhcmFnYXJ3YWwyMDIyQHZpdGJob3BhbC5hYy5pbiIsInJvbGxObyI6IjIyQkNFMTAyNDIifQ.4Wk4yvPRqww6KO9EmwaM1YAYdGxMDTFbkg_h1pHVJY4";
+        const response = await axios.get("http://20.244.56.144/test/users", {
+          headers: { Authorization: `Bearer ${access_token}` },
+        });
+        const sortedUsers = response.data.sort((a, b) => b.posts - a.posts);
+
+        setUsers(sortedUsers);
       } catch (error) {
+        console.error("Error fetching users:", error);
         setErrors(true);
       } finally {
         setLoading(false);
@@ -27,7 +33,7 @@ function TopUsers() {
   }, []);
 
   if (errors) {
-    return <div>Something went wrong</div>;
+    return <div className="text-center text-red-500">Something went wrong</div>;
   }
 
   if (loading) {
@@ -36,10 +42,14 @@ function TopUsers() {
 
   return (
     <div className="flex justify-center items-center flex-col z-0">
-      <h1 className="text-6xl underline text-center pb-5">Users</h1>
-      {/* <ul>
-          <li>{user.id}</li>
-      </ul> */}
+      <h1 className="text-6xl underline text-center pb-5">Top Users</h1>
+      <ul className="text-white text-lg">
+        {users.map((user) => (
+          <li key={user.id} className="p-2">
+            {user.name} - {user.posts} posts
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
